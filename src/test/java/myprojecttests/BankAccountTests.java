3015d32123eb3d43d109375182daa01705ee5223
Marius <mettelsi@th-brandenburg.de>
2022-07-15 00:24:34 +0000

    BankTest

---
+++ src/test/java/myprojecttests/BankAccountTests.java
@@ -0,0 +1,36 @@
+package myprojecttests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import bank.BankAccount;
+
+public class BankAccountTests {
+ 
+ @Test
+ public void deposit() {
+	 BankAccount bankaccount = new BankAccount(0);
+	 bankaccount.deposit(5000);
+	 assertEquals(5000, bankaccount.getBalance());
+
+ }
+ @Test
+ public void withdraw() {
+	 BankAccount bankaccount2 = new BankAccount(0);
+	 bankaccount2.deposit(5000);
+	 bankaccount2.withdraw(100);;
+	 assertEquals(4900, bankaccount2.getBalance());
+ }
+ @Test
+ public void withdrawForeign() {
+	 BankAccount bankaccount3 = new BankAccount(0);
+	 bankaccount3.deposit(5000);
+	 bankaccount3.withdraw(99,1.01);;
+	 assertEquals(4900, bankaccount3.getBalance());
+ }
+ public static void main(String[] args)
+{
+	 BankAccountTests b = new BankAccountTests();
+		b.deposit();
+		b.withdrawForeign();
+}
+}
